82aa5f98a2802defcbac62e4e973918abc3d3b15
Shivani Kumari <53550705+Shivani1337@users.noreply.github.com>
2020-08-24 16:01:25 +0530

    Create contact.php

---
+++ assets/forms/contact.php
@@ -0,0 +1,19 @@
+<?php
+
+$name = $_POST['name'];
+$email = $_POST['email'];
+$subject = $_POST['subject'];
+$message = $_POST['message'];
+
+$to = "connectshivani1337@gmail.com";
+$headers = "From: ".$email . "\r\n";
+
+if(mail($to,$subject,$message,$headers)){
+    http_response_code(200);
+    echo json_encode("Your message has been sent. Thank you!");
+
+}else{
+    http_response_code(200);
+    echo json_encode(array("msg" => "Something went wrong, Try Again!"));
+
+};
